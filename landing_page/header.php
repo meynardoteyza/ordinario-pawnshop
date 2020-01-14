bc9f7050d8dae6efd67a49188e38384ce722faa6
zxcjhaycee <zxcjhaycee@gmail.com>
2020-01-14 17:14:48 +0800

    Landing Page v.1

--- landing_page/header.php
+++ landing_page/header.php
@@ -24,5 +24,19 @@
   <link href="css/creative.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
-
+<style>
+    .ordinario-button{
+        background-color:#702230;
+        border-color:#702230;
+        color:#fff
+    }
+    .ordinario-button:hover{
+        color:#feb81c;
+    }
+    hr.ordinario-color{
+        max-width : 3.25rem;
+        border-width : .2rem;
+        border-color : #702230;
+    }
+</style>
 <body id="page-top">
